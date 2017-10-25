25e9142366c768b9368dc0e0c2a09ef9ebe2aa93
JoshKWilliams <32363238+JoshKWilliams@users.noreply.github.com>
2017-10-25 14:06:01 +0100

    Add files via upload

---
+++ Week 2 Exercise 6.py
@@ -0,0 +1,9 @@
+number_of_pupils = 172
+football_teams = number_of_pupils / 5
+print(number_of_pupils)
+print(football_teams)
+x = football_teams
+x = int(x)
+children_in_teams = x * 5
+children_left_out = number_of_pupils - children_in_teams
+print(children_left_out)
